2b815b99046cb0d0efaa98e2695642dcba02ee04
gopikasuku <gopikasuku@gmail.com>
2024-08-05 02:37:53 -0400

    auth routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -6,7 +6,7 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use("*",cors());
@@ -40,7 +40,7 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
-
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
